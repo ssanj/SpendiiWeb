e33b6dce14b161741486e4a8d3041b52e94389d6
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2010-12-11 18:40:27 +1000

    adding tests for MongoWriteResult

--- src/test/scala/spendii/mongo/MongoWriteResultSuite.scala
+++ src/test/scala/spendii/mongo/MongoWriteResultSuite.scala
@@ -10,7 +10,7 @@ import org.scalatest.matchers.ShouldMatchers
 
 final class MongoWriteResultSuite extends FunSuite with ShouldMatchers with MongoWriteResultTrait {
 
-  test("A MongoWriteResult should a Some(MongoError) if there is a error") {
+  test("A MongoWriteResult should return Some(MongoError) if there is a error") {
     MongoWriteResult(new WriteResultTrait {
       def getError = Some("There was an error")
       def getLastErrorTrace = Some("Some exception was thrown")
@@ -20,6 +20,26 @@ final class MongoWriteResultSuite extends FunSuite with ShouldMatchers with Mong
         me.stackTrace should equal ("Some exception was thrown")
       }
       case None => fail("Expected a MongoError")
+    }
+  }
+
+  test("A MongoWriteResult should return None if there are no errors") {
+    MongoWriteResult(new WriteResultTrait{
+      def getError = None
+      def getLastErrorTrace = Some("blah") //Even if this has a value (and it shouldn't) the error is keyed off of the getError message.
+    }).getMongoError match {
+      case Some(me) => fail("Expected None but got Some(" + me + ")")
+      case None =>
+    }
+  }
+
+  test("A MongoWriteResult should return a Some(MongoError) of the Exception, if an Exception is thrown when accessing error information") {
+    MongoWriteResult(new WriteResultTrait{
+      def getError = throw new RuntimeException("boom!")
+      def getLastErrorTrace = None
+    }).getMongoError match {
+      case Some(me) =>  me.message should equal ("boom!")
+      case None => fail("Expected a Some(MongoError) but got None")
     }
   }
 }
